adf83289a34153d87e6d38a05f81c37a1d03aec3
Blackie360 <felixkent360@gmail.com>
2024-02-16 22:27:15 +0300

    demo intro to web auth using firebase

---
+++ JAVASCRIPT/app.js
@@ -0,0 +1,63 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  import {
+    getAuth,
+    createUserWithEmailAndPassword,
+  } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDfSTE2bhf2sfhjZ4VKzdcj3j1GCUMm_nY",
+    authDomain: "vue-to-do-app-4d82b.firebaseapp.com",
+    projectId: "vue-to-do-app-4d82b",
+    storageBucket: "vue-to-do-app-4d82b.appspot.com",
+    messagingSenderId: "371674597856",
+    appId: "1:371674597856:web:cfe0ea12e8268571344385"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth()
+
+
+var fullName = document.getElementById("fullname");
+var contact = document.getElementById("contact");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var copassword = document.getElementById("copassword")
+window.signup = function (e) {
+if(password)
+
+    if(fullName.value == "" || contact.value=="" || email.value =="" || password.value ==""){
+        alert("All Field Are Required")
+    }
+    if(password.value == copassword.value){
+     
+    }
+    else{
+        alert("Password Confirmation is Wrong")
+        return false
+    }
+
+    e.preventDefault();
+    var obj = {
+      firstName: fullName.value,
+      contact: contact.value,
+      email: email.value,
+      password: password.value,
+    };
+  
+    createUserWithEmailAndPassword(auth, obj.email, obj.password)
+    .then(function(success){
+        window.location.replace('HTML/login.html')
+      // console.log(success.user.uid)
+      alert("signup successfully")
+    })
+    .catch(function(err){
+      alert("Error in " + err)
+    });
+   console.log()
+    console.log(obj);
+  };
---
+++ JAVASCRIPT/login.js
@@ -0,0 +1,53 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDfSTE2bhf2sfhjZ4VKzdcj3j1GCUMm_nY",
+  authDomain: "vue-to-do-app-4d82b.firebaseapp.com",
+  projectId: "vue-to-do-app-4d82b",
+  storageBucket: "vue-to-do-app-4d82b.appspot.com",
+  messagingSenderId: "371674597856",
+  appId: "1:371674597856:web:cfe0ea12e8268571344385"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+window.login= function(e) {
+  e.preventDefault();
+  var obj = {
+    email: email.value,
+    password: password.value,
+  };
+
+  signInWithEmailAndPassword(auth, obj.email, obj.password)
+    .then(function (success) {
+      alert("logined Successfully")
+      var aaaa =  (success.user.uid);
+      localStorage.setItem("uid",aaaa)
+      console.log(aaaa)
+      
+      
+      
+      window.location.replace('UserPage.html')
+     // localStorage.setItem(success,user,uid)
+      
+    })
+    .catch(function (err) {
+      alert("login error"+err);
+    });
+
+  console.log(obj);
+}

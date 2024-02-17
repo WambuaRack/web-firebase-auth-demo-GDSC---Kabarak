acf41c78b9c7a087bda0d1a0eccd0440a00a9919
wambuarack <shedrackwambua40@gmail.com>
2024-02-17 02:12:41 -0800

    firebase

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -1,20 +1,21 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
   import {
-    getAuth,
-    createUserWithEmailAndPassword,
+      getAuth,
+      createUserWithEmailAndPassword,
   } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyDfSTE2bhf2sfhjZ4VKzdcj3j1GCUMm_nY",
-    authDomain: "vue-to-do-app-4d82b.firebaseapp.com",
-    projectId: "vue-to-do-app-4d82b",
-    storageBucket: "vue-to-do-app-4d82b.appspot.com",
-    messagingSenderId: "371674597856",
-    appId: "1:371674597856:web:cfe0ea12e8268571344385"
+      apiKey: "AIzaSyB7KH7aSCDFf6TBeHD8piz5jYUcJ5ZhfQ4",
+      authDomain: "fir-de8d0.firebaseapp.com",
+      projectId: "fir-de8d0",
+      storageBucket: "fir-de8d0.appspot.com",
+      messagingSenderId: "413687840409",
+      appId: "1:413687840409:web:0d9e8930d35b6ede0f8208",
+      measurementId: "G-GPVPXPDPRE"
   };
 
   // Initialize Firebase
@@ -22,42 +23,41 @@
   const auth = getAuth()
 
 
-var fullName = document.getElementById("fullname");
-var contact = document.getElementById("contact");
-var email = document.getElementById("email");
-var password = document.getElementById("password");
-var copassword = document.getElementById("copassword")
-window.signup = function (e) {
-if(password)
+  var fullName = document.getElementById("fullname");
+  var contact = document.getElementById("contact");
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  var copassword = document.getElementById("copassword")
+  window.signup = function(e) {
+      if (password)
 
-    if(fullName.value == "" || contact.value=="" || email.value =="" || password.value ==""){
-        alert("All Field Are Required")
-    }
-    if(password.value == copassword.value){
-     
-    }
-    else{
-        alert("Password Confirmation is Wrong")
-        return false
-    }
+          if (fullName.value == "" || contact.value == "" || email.value == "" || password.value == "") {
+              alert("All Field Are Required")
+          }
+      if (password.value == copassword.value) {
 
-    e.preventDefault();
-    var obj = {
-      firstName: fullName.value,
-      contact: contact.value,
-      email: email.value,
-      password: password.value,
-    };
-  
-    createUserWithEmailAndPassword(auth, obj.email, obj.password)
-    .then(function(success){
-        window.location.replace('HTML/login.html')
-      // console.log(success.user.uid)
-      alert("signup successfully")
-    })
-    .catch(function(err){
-      alert("Error in " + err)
-    });
-   console.log()
-    console.log(obj);
+      } else {
+          alert("Password Confirmation is Wrong")
+          return false
+      }
+
+      e.preventDefault();
+      var obj = {
+          firstName: fullName.value,
+          contact: contact.value,
+          email: email.value,
+          password: password.value,
+      };
+
+      createUserWithEmailAndPassword(auth, obj.email, obj.password)
+          .then(function(success) {
+              window.location.replace('HTML/login.html')
+                  // console.log(success.user.uid)
+              alert("signup successfully")
+          })
+          .catch(function(err) {
+              alert("Error in " + err)
+          });
+      console.log()
+      console.log(obj);
   };
--- JAVASCRIPT/user.js
+++ JAVASCRIPT/user.js
@@ -2,12 +2,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
-import { getDatabase ,ref , push ,set, onValue,onChildAdded,get,remove,update, onChildChanged, onChildRemoved } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push, set, onValue, onChildAdded, get, remove, update, onChildChanged, onChildRemoved } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 import {
     getAuth,
     signOut,
     onAuthStateChanged,
-  } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,12 +15,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDfSTE2bhf2sfhjZ4VKzdcj3j1GCUMm_nY",
-  authDomain: "vue-to-do-app-4d82b.firebaseapp.com",
-  projectId: "vue-to-do-app-4d82b",
-  storageBucket: "vue-to-do-app-4d82b.appspot.com",
-  messagingSenderId: "371674597856",
-  appId: "1:371674597856:web:cfe0ea12e8268571344385"
+    apiKey: "AIzaSyB7KH7aSCDFf6TBeHD8piz5jYUcJ5ZhfQ4",
+    authDomain: "fir-de8d0.firebaseapp.com",
+    projectId: "fir-de8d0",
+    storageBucket: "fir-de8d0.appspot.com",
+    messagingSenderId: "413687840409",
+    appId: "1:413687840409:web:0d9e8930d35b6ede0f8208",
+    measurementId: "G-GPVPXPDPRE"
 };
 
 // Initialize Firebase
@@ -30,140 +31,141 @@ const auth = getAuth();
 const database = getDatabase();
 
 var inp = document.getElementById("Name")
-var descri= document.getElementById("Description") 
+var descri = document.getElementById("Description")
 var status = document.getElementById("Status")
 
 
-window.sendtodo = function () {
+window.sendtodo = function() {
     var obj = {
         task: inp.value,
-        description:descri.value,
-        status:status.value,
-        uid:localStorage.getItem("uid")
+        description: descri.value,
+        status: status.value,
+        uid: localStorage.getItem("uid")
     };
-       if(obj.task=="",obj.description=="",obj.status==""){
+    if (obj.task == "", obj.description == "", obj.status == "") {
         alert("All Fields Are Requried")
         return false
-       }
-       else{
+    } else {
 
-    const keyRef = ref(database, 'todotask','Users')
-    obj.id=push(keyRef).key;
-    const refrences = ref(database, `todotask/${obj.id}/`);
-    // const sab =  localStorage.setItem()
-    // console.log(sab)
-
+        const keyRef = ref(database, 'todotask', 'Users')
+        obj.id = push(keyRef).key;
+        const refrences = ref(database, `todotask/${obj.id}/`);
+        // const sab =  localStorage.setItem()
+        // console.log(sab)
 
 
-    set(refrences,obj)
+
+        set(refrences, obj)
         console.log(obj.id)
         console.log(obj)
         alert("data addded")
 
-}
+    }
 }
 var list = []
-function renderData(){
+
+function renderData() {
     const refrences = ref(database, `todotask/`);
     var parent = document.getElementById('parent')
     parent.innerHTML = "";
-    for(var i=0;  i<list.length; i++ ){
-      // console.log(list[i].uid)
-        if(list[i].uid==localStorage.getItem("uid")){
-          parent.innerHTML += `<ul><li id=${list[i].id}>Name : ${list[i].task}</br><li id=${list[i].id}>Description:${list[i].description}</br><li id=${list[i].id}>Status:${list[i].status}</br><button onclick="delTask(this)" id="del">${"Delete"}</button><button onclick="editask(this)" id="edit">${"Edit"}</button></li></ul>`;
+    for (var i = 0; i < list.length; i++) {
+        // console.log(list[i].uid)
+        if (list[i].uid == localStorage.getItem("uid")) {
+            parent.innerHTML += `<ul><li id=${list[i].id}>Name : ${list[i].task}</br><li id=${list[i].id}>Description:${list[i].description}</br><li id=${list[i].id}>Status:${list[i].status}</br><button onclick="delTask(this)" id="del">${"Delete"}</button><button onclick="editask(this)" id="edit">${"Edit"}</button></li></ul>`;
         }
     }
 }
 "user"
 var un = [];
-function username(){
 
-  var usn =  document.getElementById('user');
-  usn.innerHTML = "";
-  for (var i=0; i<un.length; i++){
-    usn.innerHTML += `<h2>${un[i].user}</h2>`
-  }
+function username() {
+
+    var usn = document.getElementById('user');
+    usn.innerHTML = "";
+    for (var i = 0; i < un.length; i++) {
+        usn.innerHTML += `<h2>${un[i].user}</h2>`
+    }
 
 }
 username();
 
-window.getdata = function () {
+window.getdata = function() {
 
-  onValue(ref(database, '/todotask/'), (snapshot) => {
-    // console.log(snapshot.val())
-  });
-
-
-  const taskRef =ref(database,'todotask/');
-  onChildAdded(taskRef, function(data){
-   list.push(data.val());
-      // console.log(data.val());
-      renderData();
-  });
-  // console.log(list.uid)
-
-}
-window.delTask = function(y){
-    let UID = y.parentElement.getAttribute("id")
-  remove(ref(database,`todotask/${UID}`))
-  .then(()=>{
-    alert("data removed successfully ")
-  })
-  .catch((error)=>{
-     alert("error"+error)
-  })
-  y.parentElement.parentElement.remove()
-}
-window.editask = function(y){
-    let Value = prompt("Please Enter new name ",y.parentElement.value)
-    let Value2 = prompt("Please Enter new description ",y.parentElement.value)
-    let Value3 = prompt("Please Enter new Status ",y.parentElement.value)
-    let UID = y.parentElement.getAttribute("id")
-  const REF = ref(database, `todotask/${UID}`);
-  update(REF,{
-    task: Value,
-    description:Value2,
-    status:Value3
-  })
-  .then(()=>{
-    alert("data Update successfully ")
-    var parent = document.getElementById('parent')
-    parent.innerHTML="";
-    getdata()
-  })
-  .catch((error)=>{
-     alert("error"+error)
-  })
-// console.log()
-  
-}
-
-
-
-window.logout = function () {
-  signOut(auth)
-    .then(function () {
-      alert("Logout Successfully");
-      window.location.href = "login.html";
-    })
-    .catch(function (err) {
-      console.log(err);
+    onValue(ref(database, '/todotask/'), (snapshot) => {
+        // console.log(snapshot.val())
     });
+
+
+    const taskRef = ref(database, 'todotask/');
+    onChildAdded(taskRef, function(data) {
+        list.push(data.val());
+        // console.log(data.val());
+        renderData();
+    });
+    // console.log(list.uid)
+
+}
+window.delTask = function(y) {
+    let UID = y.parentElement.getAttribute("id")
+    remove(ref(database, `todotask/${UID}`))
+        .then(() => {
+            alert("data removed successfully ")
+        })
+        .catch((error) => {
+            alert("error" + error)
+        })
+    y.parentElement.parentElement.remove()
+}
+window.editask = function(y) {
+    let Value = prompt("Please Enter new name ", y.parentElement.value)
+    let Value2 = prompt("Please Enter new description ", y.parentElement.value)
+    let Value3 = prompt("Please Enter new Status ", y.parentElement.value)
+    let UID = y.parentElement.getAttribute("id")
+    const REF = ref(database, `todotask/${UID}`);
+    update(REF, {
+            task: Value,
+            description: Value2,
+            status: Value3
+        })
+        .then(() => {
+            alert("data Update successfully ")
+            var parent = document.getElementById('parent')
+            parent.innerHTML = "";
+            getdata()
+        })
+        .catch((error) => {
+            alert("error" + error)
+        })
+        // console.log()
+
+}
+
+
+
+window.logout = function() {
+    signOut(auth)
+        .then(function() {
+            alert("Logout Successfully");
+            window.location.href = "login.html";
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 };
 
 function checkAuthentication() {
-  onAuthStateChanged(auth, function (user) {
-    if (user) {
-      
-      
-      const uid = user.uid;
-      console.log(uid);
-      // ...
-    } else {
-      // User is signed out
-      // ...
-      window.location.href = "login.html";
-    }
-  });
+    onAuthStateChanged(auth, function(user) {
+        if (user) {
+
+
+            const uid = user.uid;
+            console.log(uid);
+            // ...
+        } else {
+            // User is signed out
+            // ...
+            window.location.href = "login.html";
+        }
+    });
 }
 checkAuthentication();
